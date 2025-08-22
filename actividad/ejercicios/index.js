bd2353a0c08d8615d3cbc339e450ab97af9eb821
valxci <valentinacastanoibarra@gmail.com>
2025-08-22 17:35:26 -0500

    create estructure

---
+++ actividad/ejercicios/index.js
@@ -0,0 +1,12 @@
+
+class coche {
+    this.marca("Mercedes,audi,")
+    this.modelo(C220)
+    this.año(2015,2000,1990,)
+};
+
+if (año < 2000) {
+    console.log("El coche es antiguo")
+} else {
+    console.log("El coche es nuevo")
+};
